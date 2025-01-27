a8798a17e143122fc6180eb62d39b73ad7051657
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-01-27 11:35:35 +0100

    Update day9.py

--- 2016/day9.py
+++ 2016/day9.py
@@ -9,59 +9,53 @@ import re
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2016\day9.txt','r') as f:
     line=f.read()
 
-line='A(2x2)BCD(2x2)EFG'
+# line='A(2x2)BCD(2x2)EFG'
+# line='A(1x5)BC'
+# line='(6x1)(1x3)A'
+# line='X(8x2)(3x3)ABCY'
+# line='ADVENT'
 
-cpt=0
-new_line=''
+# brac=re.search('\((\d+)x(\d+)\)',line)
 
-while cpt<len(line):
-    brackets = re.search(r'\((\d+)x(\d+)\)', line[cpt:])
-    if not brackets:
-        new_line+=line[cpt:]
-        break
-    pos=brackets.start(0)
-    new_line+=line[cpt:pos]
-    sz=int(brackets.group(1))
-    rpt=int(brackets.group(2))
-    pos + len(brackets.group())
-    
-line[0:1]
+# Me
+def rec_len(l):
+    brac=re.search('\((\d+)x(\d+)\)',l)
+    if not brac:
+        return l
+    pos=brac.start()
+    size=int(brac.group(1))
+    rpt=int(brac.group(2))
+    end=brac.end()
+    return l[:pos] + rpt*l[end:end+size] + rec_len(l[end+size:])
 
+# LieutenantSwr2d2
+def rec_len_sol(l):
+    brac=re.search('\((\d+)x(\d+)\)',l)
+    if not brac:
+        return len(l)
+    pos=brac.start()
+    size=int(brac.group(1))
+    rpt=int(brac.group(2))
+    end=brac.end()
+    return len(l[:pos]) + rpt*size + rec_len_sol(l[end+size:])
 
+def rec_len_sol_2(l):
+    brac=re.search('\((\d+)x(\d+)\)',l)
+    if not brac:
+        return len(l)
+    pos=brac.start()
+    size=int(brac.group(1))
+    rpt=int(brac.group(2))
+    end=brac.end()
+    return len(l[:pos]) + rpt*rec_len_sol_2(l[end:end+size]) + rec_len_sol_2(l[end+size:])
 
-    if line[cpt]=='(' and line[cpt+2]=='x' and line[cpt+4]==')':
-        for i in range(int(line[cpt+3])):
-            new_line+=line[cpt+5:cpt+5+int(line[cpt+1])]
-        cpt+=(5+int(line[cpt+1]))
-    else:
-        new_line+=line[cpt]
-        cpt+=1
-
-print(len(new_line))
-
-length=0
-while cpt<len(line):
-    if line[cpt]=='(' and line[cpt+2]=='x' and line[cpt+4]==')':
-        length+=(int(line[cpt+3])*int(line[cpt+1]))
-        cpt+=5+int(line[cpt+1])
-    else:
-        length+=1
-        cpt+=1
-
-print(len(new_line))
+print(rec_len_sol(line))
+print(rec_len_sol_2(line))
 
 
+# line='(3x3)XYZ'
+# line='X(8x2)(3x3)ABCY'
+# line='(27x12)(20x12)(13x14)(7x10)(1x12)A'
+# line='X(8x2)(3x3)ABCY'
+# line='ADVENT'
 
-def day9a(d):
-    bracket = re.search(r'\((\d+)x(\d+)\)', d)
-    if not bracket:
-        return len(d)
-    pos = brackets.start(0)
-    sz = int(brackets.group(1))
-    rpt = int(brackets.group(2))
-    i = pos + len(brackets.group())
-    return len(d[:pos]) + len(d[i:i+sz]) * rpt + day9a(d[i+sz:])
-
-day9a(line)
-
-brackets = re.search(r'\((\d+)x(\d+)\)', line[cpt:])
