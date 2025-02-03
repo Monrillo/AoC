2bd47e811c5d0e0435408bd8bdda09991873eb43
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-02-03 15:12:48 +0100

    Update day14.py

--- 2016/day14.py
+++ 2016/day14.py
@@ -5,40 +5,26 @@ Created on Fri Jan 31 11:06:34 2025
 @author: castelf
 """
 
-import hashlib
+from hashlib import md5
 import re
 #import numpy as np
 
-salt='abc'
+#salt='abc'
+salt='ihaygndm'
 n=0
 triple=[]
 key_pad=[]
+debug=[]
 
 while len(key_pad)<64:
     string=salt+str(n)
-    h = hashlib.md5(string.encode()).hexdigest()
+    h = md5(string.encode()).hexdigest()
+    for _ in range(2016): h=md5(h.encode()).hexdigest()
     trip=re.search(r'(\w)(\1)(\1)',h)
     for key in triple:
-        if key[0]*5 in h: key_pad.append(key[1]);triple.remove(key);continue
-        if (n-key[1])>1000: triple.remove(key)
+        if (key[0]*5 in h) and ((n-key[1])<=1000): key_pad.append(key[1]);debug.append((key,n,(n-key[1])))
     if trip: triple.append([trip.group()[0],n])
     n+=1
     
-print(sorted(key_pad))
-len(key_pad)
+print(sorted(key_pad)[63])
 
-
-for i in range(1000):
-    string=salt+str(n)
-    h = hashlib.md5(string.encode()).hexdigest()
-    trip=re.search(r'(\w)(\1)(\1)',h)
-    for key in triple:
-        if key[0]*5 in h: key_pad.append(key);triple.remove(key);continue
-        if (n-key[1])>1000: triple.remove(key)
-    if trip: triple.append([trip.group()[0],n])
-    n+=1
-
-
-string=salt+str(39)
-h = hashlib.md5(string.encode()).hexdigest()
-print(h)
