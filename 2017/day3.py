6926022f2130a5d4e40864f635cc1e71cb98808b
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-02-20 17:41:40 +0100

    Create day3.py

---
+++ 2017/day3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 16:49:23 2025
+
+@author: castelf
+"""
+
+goal=1024
+
+odd=1
+while True:
+    if odd**2>goal: break
+    odd+=1
+print(odd)
+33**2
