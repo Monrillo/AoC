86b155774b0b2559a1ebfdd2c1dac0718a1dde56
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-03-07 16:08:54 +0100

    Create day10.py

---
+++ 2017/day10.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 15:10:25 2025
+
+@author: castelf
+"""
+import numpy as np
+
+# circ=np.arange(256)
+# length=np.array([206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3])
+
+circ=np.arange(5)
+length=np.array([3,4,1,5])
+
+skip=0
+
+print(circ)
+np.flip(circ)
