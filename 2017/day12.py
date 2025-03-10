8e36f79da4ef251862e647994446f799482145eb
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-03-10 16:05:48 +0100

    Create day12.py

---
+++ 2017/day12.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 15:08:50 2025
+
+@author: castelf
+"""
+
+import numpy as np
+
+lines=open('C:\\Users\castelf\Documents\GitHub\AoC\\2017\day12.txt','r').readlines()
+for line in lines: line.split(' <-> ')
