fbdc22faf6b045f9c39ebf28a90abb88734ef886
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-03-11 13:40:04 +0100

    Update day12.py

--- 2017/day12.py
+++ 2017/day12.py
@@ -5,7 +5,23 @@ Created on Mon Mar 10 15:08:50 2025
 @author: castelf
 """
 
-import numpy as np
+import networkx as nx
 
 lines=open('C:\\Users\castelf\Documents\GitHub\AoC\\2017\day12.txt','r').readlines()
-for line in lines: line.split(' <-> ')
+
+# lines=['0 <-> 2',
+# '1 <-> 1',
+# '2 <-> 0, 3, 4',
+# '3 <-> 2, 4',
+# '4 <-> 2, 3, 6',
+# '5 <-> 6',
+# '6 <-> 4, 5']
+
+g=nx.Graph()
+for line in lines:
+    val=line.split(' <-> ')
+    g.add_node(int(val[0]))
+    for i in val[1].split(', '): g.add_edge(int(val[0]),int(i))
+
+print(len(nx.node_connected_component(g,0)))
+print(nx.number_connected_components(g))
