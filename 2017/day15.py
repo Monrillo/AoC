a92133d9975c84c5b56113fc5804345858798c89
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-03-12 15:52:30 +0100

    Update day15.py

--- 2017/day15.py
+++ 2017/day15.py
@@ -12,14 +12,38 @@ import time
 a=516
 b=190
 
+# start=time.time()
+
+# res=0
+# for _ in range(int(4e7)):
+#     a=(a*16807)%2147483647
+#     b=(b*48271)%2147483647
+#     if bin(a)[-16:]==bin(b)[-16:]: res+=1
+
+# end=time.time()
+# print(res)
+# print(end-start)
+
+def gena(a):
+    while True:
+        a=(a*16807)%2147483647
+        if a%4==0: break
+    return a
+
+def genb(b):
+    while True:
+        b=(b*48271)%2147483647
+        if b%8==0: break
+    return b
+
 start=time.time()
 
-res=0
-for _ in range(int(4e7)):
-    a=(a*16807)%2147483647
-    b=(b*48271)%2147483647
-    if bin(a)[-16:]==bin(b)[-16:]: res+=1
+res2=0
+for _ in range(int(5e6)):
+    a=gena(a)
+    b=genb(b)
+    if bin(a)[-16:]==bin(b)[-16:]: res2+=1
 
 end=time.time()
-print(res)
+print(res2)
 print(end-start)
