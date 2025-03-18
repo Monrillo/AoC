cf89e3ab6e08785723a008e37108121e525a1998
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-03-18 16:20:03 +0100

    Update day20.py

--- 2017/day20.py
+++ 2017/day20.py
@@ -15,8 +15,17 @@ particles=[[int(x) for x in val] for val in values]
 # values=[re.findall('(-?\d+)', line) for line in lines]
 # particles=[[int(x) for x in val] for val in values]
 
+# lines=['p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>',
+# 'p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>',
+# 'p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>',
+# 'p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>']
+# values=[re.findall('(-?\d+)', line) for line in lines]
+# particles=[[int(x) for x in val] for val in values]
+
 d=[0]*len(particles)
-for _ in range(1000):
+col=[False]*len(particles)
+for _ in range(500):
+    pos=[]
     for i,p in enumerate(particles):
         p[3]+=p[6]
         p[4]+=p[7]
@@ -25,4 +34,9 @@ for _ in range(1000):
         p[1]+=p[4]
         p[2]+=p[5]
         d[i]=abs(p[0])+abs(p[1])+abs(p[2])
+        pos.append((p[0],p[1],p[2]))
+    for i,po in enumerate(pos):
+        if pos.count(po)>1: col[i]=True
 print(d.index(min(d)))
+print(len(particles)-sum(col))
+
