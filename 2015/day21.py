c9d1f9868bb79eef290b3f460b90b317185b7b82
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-03-21 10:56:00 +0100

    Update day21.py

--- 2015/day21.py
+++ 2015/day21.py
@@ -5,6 +5,8 @@ Created on Thu Mar 20 11:37:05 2025
 @author: castelf
 """
 
+import itertools as it
+
 # Weapons:    Cost  Damage  Armor
 # Dagger        8     4       0
 # Shortsword   10     5       0
@@ -32,19 +34,41 @@ class Object:
         self.damage = damage
         self.armor = armor
         self.cost = cost
+    
+    def get_cost(self): return self.cost
+        
+Dagger=Object(8,4,0)
+Shortsword=Object(10,5,0)
+Warhammer=Object(25,6,0)
+Longsword=Object(40,7,0)
+Greataxe=Object(74,8,0)
 
-class Weapon(Object):
-    def __init__(self):
-        super().__init__()
+Weapons=[Dagger,Shortsword,Warhammer,Longsword,Greataxe]
 
-class Armor(Object):
-    def __init__(self):
-        super().__init__()
+No_armor=Object(0,0,0)
+Leather=Object(13,0,1)
+Chainmail=Object(31,0,2)
+Splintmail=Object(53,0,3)
+Bandedmail=Object(75,0,4)
+Platemail=Object(102,0,5)
 
-class Ring(Object):
-    def __init__(self):
-        super().__init__()
+Armor=[No_armor,Leather,Chainmail,Splintmail,Bandedmail,Platemail]
 
+No_ring_l=Object(0,0,0)
+No_ring_r=Object(0,0,0)
+Dam_1=Object(25,1,0)
+Dam_2=Object(50,2,0)
+Dam_3=Object(100,3,0)
+Def_1=Object(20,0,1)
+Def_2=Object(40,0,2)
+Def_3=Object(80,0,3)
+
+Rings=[No_ring_l,No_ring_r,Dam_1,Dam_2,Dam_3,Def_1,Def_2,Def_3]
+Ring_1=[]
+Ring_2=[]
+for a,b in it.combinations(Rings, 2):Ring_1.append(a);Ring_2.append(b)
+
+combinations = it.product(Weapons,Armor,Ring_1,Ring_2)
 
 class Person:
     def __init__(self, damage, armor, health):
@@ -52,7 +76,37 @@ class Person:
         self.armor = armor
         self.health = health
     
-    def attack(self, Person):
-        Person.health-=self.damage-Person.armor
+    def hit(self, dam): self.health-=max(dam-self.armor,1)
+    
+    def attack(self): return self.damage
+    
+    def alive(self): return self.health>0
 
-test=Person()
+
+class Human(Person):
+    def equip(self, Object):
+        self.damage+=Object.damage
+        self.armor+=Object.armor
+
+winning=[]
+lost=[]
+for comb in combinations:
+    cost=0
+    boss=Person(8,1,104)
+    player=Human(0,0,100)
+    for item in comb:
+        player.equip(item)
+        cost+=item.get_cost()
+        
+    while True:
+        boss.hit(player.attack())
+        if not boss.alive(): win=True;break
+        elif boss.alive():
+            player.hit(boss.attack())
+            if not player.alive(): win=False;break
+    
+    if win:winning.append(cost)
+    else: lost.append(cost)
+
+print(min(winning))
+print(max(lost))
