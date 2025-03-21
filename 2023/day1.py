849bd553266c110324499eeaa15ede9b0de91ae6
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-03-21 11:34:42 +0100

    Update day1.py

--- 2023/day1.py
+++ 2023/day1.py
@@ -9,7 +9,7 @@ import re
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2023\day1.txt','r') as f: lines=f.readlines()
 
-nums=[''.join(re.findall('(\d+)', line)) for line in lines]
+nums=[''.join(re.findall('(\d+)', line.strip())) for line in lines]
 res=0
 for num in nums: res+=int(num[0]+num[-1])
 print(res)
