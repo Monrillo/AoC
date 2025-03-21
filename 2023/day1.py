ed82d3506be0ff80b8c82a77bb24fef9afe18a97
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-03-21 11:27:46 +0100

    Create day1.py

---
+++ 2023/day1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 21 11:01:43 2025
+
+@author: castelf
+"""
+
+import re
+
+with open('C:\\Users\castelf\Documents\GitHub\AoC\\2023\day1.txt','r') as f: lines=f.readlines()
+
+nums=[''.join(re.findall('(\d+)', line)) for line in lines]
+res=0
+for num in nums: res+=int(num[0]+num[-1])
+print(res)
+
