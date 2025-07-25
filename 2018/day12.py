95eac3a1667b2723cbeec89a7eace9ad7ab0989a
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-07-25 16:13:00 +0200

    Update day12.py

--- 2018/day12.py
+++ 2018/day12.py
@@ -25,70 +25,37 @@ with open('C:\\Users\castelf\Documents\GitHub\AoC\\2018\day12.txt','r') as f: li
 # '####. => #']
 
 # Initialisation
-state='.....'+lines[0].strip()[15:]+'.....................................'
-print(state)
+state=lines[0].strip()[15:]
+plant=set(i for i,n in enumerate(state) if n =='#')
+print('0',sum(plant))
 
 # Recipe
 mod=[l.split(' => ')[0] for l in lines[2:] if l.strip().split(' => ')[1]=='#']
 
-for j in range(2000):
-    new_state='.'*len(state)
-    for i,(a,b,c,d,e) in enumerate(zip(state,state[1:],state[2:],state[3:],state[4:])):
-        pat=''.join((a,b,c,d,e))
-        if pat in mod: new_state=new_state[:i+2]+'#'+new_state[i+3:]
-    state=new_state
-    plant=set(i-5 for i,n in enumerate(state) if n =='#')
+# Computation of next generation
+def nextgen(p,m):
+    st=min(p)
+    end=max(p)
+    np=set()
+    for i in range(st-5,end+5):
+        pat=''.join('#' if i+k in p else '.' for k in [-2,-1,0,1,2])
+        if pat in m: np.add(i)
+    return np
+
+# Part 1
+for j in range(1,21):
+    plant=nextgen(plant,mod)
     print(j,sum(plant))
 
+# Part 2
 
+# for j in range(1,201):
+#     plant=nextgen(plant,mod)
+#     print(j,sum(plant))
 
+# 23 every generation, since generation 162
+#  4958 at generation 200
 
+part2=(5e10-200)*23+4958
+print(part2)
 
-
-
-
-#############################################################
-
-def nextg(cur, recipe):
-  start = min(cur)
-  end = max(cur)
-  x = set()
-
-  for i in range(start - 3, end + 4):
-    pat = ''.join('#' if i + k in cur else '.' for k in [-2, -1, 0, 1, 2])
-    if pat in recipe:
-      x.add(i)
-
-  return x
-
-def viz(cur):
-  print (''.join('#' if i in cur else '.' for i in range(-5, 120)))
-
-#with open('day12test.txt') as f:
-with open('C:\\Users\castelf\Documents\GitHub\AoC\\2018\day12.txt','r') as f:
-  lines = [l.rstrip('\n') for l in f]
-  print (lines)
-
-  init = lines[0][len('initial state: '):]
-  recipe = set()
-  for l in lines[2:]:
-    if l[-1] == '#':  # I forgot this line the first time around.
-      recipe.add(l[:5]) 
-
-  cur = set(i for i, c in enumerate(init) if c == '#')
-
-  # Part 1:
-  for i in range(20):
-    cur = nextg(cur, recipe)
-  print (sum(cur))
-
-  # Part 2:
-  # ls = 0
-  # # viz(cur)
-  # for i in range(2000):
-  #   cur = nextg(cur, recipe)
-  #   # viz(cur)
-  #   s = sum(cur)
-  #   print (i, s, s - ls)
-  #   ls = s
-  # print (sum(cur))
