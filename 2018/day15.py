19714702eb8135b09a212fce9745213872dab49d
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-08-25 15:54:46 +0200

    Create day15.py

---
+++ 2018/day15.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 22 17:07:34 2025
+
+@author: castelf
+"""
+
+with open('C:\\Users\castelf\Documents\GitHub\AoC\\2018\day15.txt','r') as f: lines=f.readlines()
+
+import numpy as np
+
+test=np.loadtxt('C:\\Users\castelf\Documents\GitHub\AoC\\2018\day15.txt',dtype=str)
+
+lines=[
+'#######',
+'#.G...#',
+'#...EG#',
+'#.#.#G#',
+'#..G#E#',
+'#.....#', 
+'#######']
