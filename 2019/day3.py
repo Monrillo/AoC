3a28587c14f95ba343987e6d8c45318a28521ed5
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-08-29 10:33:40 +0200

    Update day3.py

--- 2019/day3.py
+++ 2019/day3.py
@@ -8,8 +8,11 @@ from collections import deque
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2019\day3.txt','r') as f: lines=f.readlines()
 
-lines=['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
-'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']
+# lines=['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+# 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']
+
+# lines=['R8,U5,L5,D3',
+# 'U7,R6,D4,L4']
 
 line1=lines[0].strip().split(',')
 line2=lines[1].strip().split(',')
@@ -18,7 +21,7 @@ wire1=deque()
 wire2=deque()
 
 pos=(0,0)
-wire1.append(pos)
+#wire1.append(pos)
 for dep in line1:
     if dep[0]=='L':
         for i in range(int(dep[1:])):pos=(pos[0],pos[1]-1);wire1.append(pos)
@@ -30,7 +33,7 @@ for dep in line1:
         for i in range(int(dep[1:])):pos=(pos[0]-1,pos[1]);wire1.append(pos)
 
 pos=(0,0)
-wire2.append(pos)
+#wire2.append(pos)
 for dep in line2:
     if dep[0]=='L':
         for i in range(int(dep[1:])):pos=(pos[0],pos[1]-1);wire2.append(pos)
@@ -41,6 +44,8 @@ for dep in line2:
     elif dep[0]=='D':
         for i in range(int(dep[1:])):pos=(pos[0]-1,pos[1]);wire2.append(pos)
 
-cross=[x for x in wire1 if x in wire2]
-print(min([abs(c[0])+abs(c[1]) for c in cross[1:]]))
-print(min([list(wire1).index(c)+list(wire1).index(c) for c in cross[1:]]))
+#cross=[x for x in set(wire1) if x in set(wire2)]
+cross=[x for x in set(wire1).intersection(wire2)]
+
+print(min([abs(c[0])+abs(c[1]) for c in cross]))
+print(min([wire1.index(c)+wire2.index(c)+2 for c in cross]))
