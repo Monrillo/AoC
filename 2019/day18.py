32c1c014bfbb6663917c4186a383b61a3f15062f
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-09-19 09:39:22 +0200

    Update day18.py

--- 2019/day18.py
+++ 2019/day18.py
@@ -4,7 +4,9 @@ Created on Thu Sep 11 11:53:01 2025
 
 @author: castelf
 """
-import networkx as nx
+# import networkx as nx
+# G = nx.Graph()
+
 import numpy as np
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2019\day18.txt','r') as f: lines=f.readlines()
@@ -27,6 +29,17 @@ key_list=[]
 for k in keys:
     if len(np.where(matrix==k)[0])>0:key_list.append((np.where(matrix==k)[0][0],np.where(matrix==k)[1][0]))
 
-G = nx.Graph()
+dep=key_list[0]
+links=[[dep]]
+mat=matrix.copy()
 
-
+possible_links=[]
+for l in links:
+    p=l[-1]
+    for m in [(p[0]+1,p[1]),(p[0]-1,p[1]),(p[0],p[1]+1),(p[0],p[1]-1)]:
+        if mat[m] in keys or mat[m]=='.':
+            path=l.copy()
+            path.append(m)
+            possible_links.append(path)
+            mat[m]='#'
+links=possible_links        
