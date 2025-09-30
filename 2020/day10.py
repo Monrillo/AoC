806a87c2f3bbb409c924033c9bf89579d3ebbc6f
Florent Castellani <61456226+Monrillo@users.noreply.github.com>
2025-09-30 16:24:20 +0200

    Update day10.py

--- 2020/day10.py
+++ 2020/day10.py
@@ -21,37 +21,37 @@ with open('C:\\Users\castelf\Documents\GitHub\AoC\\2020\day10.txt','r') as f: li
 # '12',
 # '4']
 
-lines=['28',
-'33',
-'18',
-'42',
-'31',
-'14',
-'46',
-'20',
-'48',
-'47',
-'24',
-'23',
-'49',
-'45',
-'19',
-'38',
-'39',
-'11',
-'1',
-'32',
-'25',
-'35',
-'8',
-'17',
-'7',
-'9',
-'4',
-'2',
-'34',
-'10',
-'3']
+# lines=['28',
+# '33',
+# '18',
+# '42',
+# '31',
+# '14',
+# '46',
+# '20',
+# '48',
+# '47',
+# '24',
+# '23',
+# '49',
+# '45',
+# '19',
+# '38',
+# '39',
+# '11',
+# '1',
+# '32',
+# '25',
+# '35',
+# '8',
+# '17',
+# '7',
+# '9',
+# '4',
+# '2',
+# '34',
+# '10',
+# '3']
 
 jolts=np.array(sorted([int(l.strip()) for l in lines]))
 jolts=np.append(0,jolts)
@@ -59,13 +59,16 @@ jolts=np.append(jolts,jolts[-1]+3)
 num,counts=np.unique(np.diff(jolts), return_counts=True)
 print("Part 1:",counts[0]*counts[1])
 
-prod=[]
-n=-1
-for e in np.diff(jolts):
-    if e==1:n+=1
-    else: prod.append(n);n=-1
-realprod=[i for i in prod if i!=-1]
+# Graph
+graph={}
+for x in jolts:
+    graph[x]=set([i for i in range(x+1,x+4) if i in jolts])
 
-res=1
-for p in realprod: res*=2**p
-res/2
+# Depth first counter (DFS)
+def dfc(G,v,M={}):
+    if v in M: return M[v]
+    elif G[v]: M[v]=sum(dfc(G,x,M) for x in G[v]);return M[v]
+    else: return 1
+
+print("Part 2:",dfc(graph,0))
+
