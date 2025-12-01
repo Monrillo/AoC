1dec3d2df37edd35db3426d2ab5f1f82de72e684
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-12-01 17:00:15 +0100

    Update day23.py

--- 2020/day23.py
+++ 2020/day23.py
@@ -7,23 +7,46 @@ Created on Fri Nov 28 14:29:25 2025
 order='389125467'
 
 cups=[int(x) for x in list(order)]
+length=len(cups)
 maxi=max(cups)
 mini=min(cups)
 pos=0
 
-num=cups[pos]
-pick=cups[pos+1:pos+4]
-del cups[pos+1:pos+4]
 
-while num>mini-1:
-    num-=1
-    if num in cups:break
-    elif num==mini:num=maxi
 
-cups[cups.index(num)+1:1]=pick
-pos+=1
+for n in range(1,11):
+    num=cups[pos]
+    pos_num=cups[pos]
+    
+    if pos<length-4:
+        pick=cups[pos+1:pos+4]
+    else:
+        pick=cups[pos+1:pos+4]+cups[0:(pos+4)%length]
+    
+    while num>mini:
+        num-=1
+        if num not in pick:break
+        elif num<=mini:num=maxi+1
+    
+    print("-- move ",n," --")
+    print(cups)
+    print("Pick up:",pick)
+    print("Destination:",num)
+    
+    if pos<length-4:
+        del cups[pos+1:pos+4]
+    else:
+        del cups[pos+1:pos+4]
+        del cups[0:(pos+4)%length]
+    
+    cups[cups.index(num)+1:1]=pick
+    pos=cups.index(pos_num)
+    pos=(pos+1)%length
+    
+    
 
-print(cups)
-print("Pick up:",pick)
-print("Destination:",num)
-print("Position:",pos)
+
+
+
+
+
