9bf0251f7535bd7c557e6a75d10a52e2e0259662
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-12-05 14:26:16 +0100

    Update day4.py

--- 2025/day4.py
+++ 2025/day4.py
@@ -9,29 +9,37 @@ import numpy as np
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2025\day4.txt','r') as f: lines=f.readlines()
 
-lines=['..@@.@@@@.',
-'@@@.@.@.@@',
-'@@@@@.@.@@',
-'@.@@@@..@.',
-'@@.@@@@.@@',
-'.@@@@@@@.@',
-'.@.@.@.@@@',
-'@.@@@.@@@@',
-'.@@@@@@@@.',
-'@.@.@@@.@.']
+# lines=['..@@.@@@@.',
+# '@@@.@.@.@@',
+# '@@@@@.@.@@',
+# '@.@@@@..@.',
+# '@@.@@@@.@@',
+# '.@@@@@@@.@',
+# '.@.@.@.@@@',
+# '@.@@@.@@@@',
+# '.@@@@@@@@.',
+# '@.@.@@@.@.']
 
 mat=[list(l.strip()) for l in lines]
 matrix=np.array(mat)
 extend_matrix=np.pad(matrix, ((1, 1), (1, 1)), 'constant', constant_values='.')
-rolls=[(x,y) for x in np.where(extend_matrix=='@')[0] for y in np.where(extend_matrix=='@')[1]]
+rolls=[(np.where(extend_matrix=='@')[0][i],np.where(extend_matrix=='@')[1][i]) for i in range(len(np.where(extend_matrix=='@')[0]))]
 
-for roll in rolls:
-    x,y=roll
-    for dx in [-1,0,1]:
-        for dy in [-1,0,1]:
-            print((x+dx,y+dy))
+def neigh(pos):
+    x,y=pos
+    return len(np.where(extend_matrix[x-1:x+2,y-1:y+2]=='@')[0])-1
+
+forklift=[r for r in rolls if neigh(r)<=3]
             
-    
+print("Part 1:",len(forklift))
 
-sum([(x+dx,y+dy) for dx in [-1,0,1] for dy in [-1,0,1]])
-for dx in [-1,0,1]:print(dx)
+total=len(forklift)
+
+while forklift!=[]:
+    for f in forklift: extend_matrix[f]='.'
+    rolls=[(np.where(extend_matrix=='@')[0][i],np.where(extend_matrix=='@')[1][i]) for i in range(len(np.where(extend_matrix=='@')[0]))]
+    forklift=[r for r in rolls if neigh(r)<=3]
+    total+=len(forklift)
+
+print("Part 2:",total)
+
