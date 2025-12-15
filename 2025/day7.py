43c6352f38b0283a4fcf54307843860f3f67a46f
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-12-15 11:12:26 +0100

    Update day7.py

--- 2025/day7.py
+++ 2025/day7.py
@@ -7,6 +7,10 @@ Created on Tue Dec  9 13:55:11 2025
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2025\day7.txt','r') as f: lines=f.readlines()
 
+# def splitter(b,l):
+#     if l==len(lines): return 1
+#     elif lines[l].strip()[b]=='^': return splitter(b-1,l+1)+splitter(b+1,l+1)
+#     else: return splitter(b,l+1)
 
 # lines=['.......S.......',
 # '...............',
@@ -25,25 +29,17 @@ with open('C:\\Users\castelf\Documents\GitHub\AoC\\2025\day7.txt','r') as f: lin
 # '.^.^.^.^.^...^.',
 # '...............']
 
-beams=[lines[0].strip().index('S')]
+
 num_split=0
-double=0
-
+beam=[0]*len(lines[0].strip())
+beam[lines[0].strip().index('S')]+=1
 for l in lines[1:]:
-    split=[b for b in beams if l.strip()[b]=='^']
-    num_split+=len(split)
-    for s in split:
-        beams.remove(s)
-        if s+1 in beams:double+=1
-        else: beams.append(s+1)
-        if s-1 in beams:double+=1
-        else: beams.append(s-1)
-
+    for i in range(len(beam)):
+        if l.strip()[i]=='^' and beam[i]>0:
+            num_split+=1
+            beam[i-1]+=beam[i]
+            beam[i+1]+=beam[i]
+            beam[i]=0
 print("Part 1:",num_split)
+print("Part 2:",sum(beam))
 
-def splitter(b,l):
-    if l==len(lines): return 1
-    elif lines[l].strip()[b]=='^': return splitter(b-1,l+1)+splitter(b+1,l+1)
-    else: return splitter(b,l+1)
-
-print("Part 2:",splitter(lines[0].strip().index('S'),0))
