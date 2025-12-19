8bab908fd098a5cd3f8b3d47bf5be560f1f455a6
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2025-12-19 10:06:02 +0100

    Create day12.py

---
+++ 2025/day12.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 09:25:22 2025
+
+@author: castelf
+"""
+
+import numpy as np
+
+with open('C:\\Users\castelf\Documents\GitHub\AoC\\2025\day12.txt','r') as f: lines=f.readlines()
+
+lines=['0:',
+'###',
+'##.',
+'##.',
+'',
+'1:',
+'###',
+'##.',
+'.##',
+'',
+'2:',
+'.##',
+'###',
+'##.',
+'',
+'3:',
+'##.',
+'###',
+'##.',
+'',
+'4:',
+'###',
+'#..',
+'###',
+'',
+'5:',
+'###',
+'.#.',
+'###',
+'',
+'4x4: 0 0 0 0 2 0',
+'12x5: 1 0 1 0 2 2',
+'12x5: 1 0 1 0 3 2']
+
+for line in lines:
+    
+
