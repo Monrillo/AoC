3cc6354477c4b150bf6fb751e928d1a6e30c771e
Castellani Florent (TELESPAZIO) <castelf@cnes.fr>
2026-01-05 14:34:25 +0100

    Update day3.py

--- 2021/day3.py
+++ 2021/day3.py
@@ -7,6 +7,19 @@ Created on Wed Dec 24 11:44:23 2025
 
 with open('C:\\Users\castelf\Documents\GitHub\AoC\\2021\day3.txt','r') as f: lines=f.readlines()
 
+# lines=['00100',
+# '11110',
+# '10110',
+# '10111',
+# '10101',
+# '01111',
+# '00111',
+# '11100',
+# '10000',
+# '11001',
+# '00010',
+# '01010']
+
 def bin_to_dec(l):
     return sum([2**i for i,e in enumerate(l[::-1]) if e=='1'])
 
@@ -24,6 +37,40 @@ gamma_dec=bin_to_dec(gamma)
 
 print("Part 1:",epsilon_dec*gamma_dec)
 
-data2=data.copy()
-possib=range(len(data[0]))
-for i in possib:
+data2=[list(d) for d in data]
+oxygen=''
+while data2:
+    d=data2.pop(0)
+    if len(d)>1:
+        common='1' if d.count('1')>=d.count('0') else '0'
+        ignore=[i for i,x in enumerate(d) if x!=common]
+    else:
+        common=d[0]
+        ignore=[]
+    oxygen+=common
+    
+    for j in range(len(data2)):
+        for i in ignore[::-1]:
+            data2[j].pop(i)
+
+data2=[list(d) for d in data]
+co=''
+while data2:
+    d=data2.pop(0)
+    if len(d)>1:
+        least='0' if d.count('0')<=d.count('1') else '1'
+        ignore=[i for i,x in enumerate(d) if x!=least]
+    else:
+        least=d[0]
+        ignore=[]
+    co+=least
+    
+    for j in range(len(data2)):
+        for i in ignore[::-1]:
+            data2[j].pop(i)
+
+oxygen_dec=bin_to_dec(oxygen)
+co_dec=bin_to_dec(co)
+
+print("Part 2:",oxygen_dec*co_dec)
+
